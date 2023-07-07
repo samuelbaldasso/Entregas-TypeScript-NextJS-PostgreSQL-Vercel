5bd79df4a745c1c67c2b0a49fc27d0a2c84be44c
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-07-07 17:39:12 -0300

    hotfix: request get e post do json

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -10,30 +10,59 @@
 //   await fs.writeFile(filePath, jsonData);
 // };
 
+// const getFormData = async () => {
+//   return await fs.readFile(filePath + '/data.json', 'utf8');
+// };
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'POST') {
+//     const data = req.body;
+//     saveFormData(data);
+//     res.status(200).json({ message: 'Dados salvos com sucesso.' });
+//   } else if (req.method === 'GET') {
+//     const data = await getFormData().then((e) => {
+//       return JSON.parse(e);
+//     });
+//     res.status(200).json(data);
+//   } else {
+//     res.status(405).json({ message: 'Método não permitido.' });
+//   }
+// }
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs/promises';
 import path from 'path';
 
-const filePath = path.join(process.cwd(), '/src/json');
+interface FormData {
+  formData: any[];
+}
 
-const saveFormData = async (data: string) => {
-  data = await getFormData();
-  await fs.writeFile(filePath, data);
+const filePath = path.join(process.cwd(), '/src/json/data.json');
+
+const saveFormData = async (data: any) => {
+  const existingData = await getFormData();
+  const formData: FormData = { formData: [...existingData, data] };
+  const jsonData = JSON.stringify(formData);
+  await fs.writeFile(filePath, jsonData);
 };
 
-const getFormData = async () => {
-  return await fs.readFile(filePath + '/data.json', 'utf8');
+const getFormData = async (): Promise<any[]> => {
+  try {
+    const jsonData = await fs.readFile(filePath, 'utf8');
+    const formData: FormData = JSON.parse(jsonData);
+    return formData.formData || [];
+  } catch (error) {
+    return [];
+  }
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const data = req.body;
-    await saveFormData(data);
+    saveFormData(data);
     res.status(200).json({ message: 'Dados salvos com sucesso.' });
   } else if (req.method === 'GET') {
-    const data = await getFormData().then((e) => {
-      return JSON.parse(e);
-    });
+    const data = await getFormData();
     res.status(200).json(data);
   } else {
     res.status(405).json({ message: 'Método não permitido.' });
