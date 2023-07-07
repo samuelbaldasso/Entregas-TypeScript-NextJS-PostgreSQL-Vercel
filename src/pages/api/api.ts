e8aba3341e09f9a0fcef0dd3a52b4c0d33a9eac7
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-07-07 17:58:35 -0300

    hotfix: request get e post do json

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,59 +1,20 @@
-// import { promises as fs } from 'fs';
-// import { NextApiRequest, NextApiResponse } from 'next';
-
-// const path = require("path");
-
-// const filePath = path.join(process.cwd(), "/src/json");
-
-// const saveFormData = async (data: any) => {
-//   const jsonData = JSON.stringify({ formData: [...await getFormData(), data] });
-//   await fs.writeFile(filePath, jsonData);
-// };
-
-// const getFormData = async () => {
-//   return await fs.readFile(filePath + '/data.json', 'utf8');
-// };
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method === 'POST') {
-//     const data = req.body;
-//     saveFormData(data);
-//     res.status(200).json({ message: 'Dados salvos com sucesso.' });
-//   } else if (req.method === 'GET') {
-//     const data = await getFormData().then((e) => {
-//       return JSON.parse(e);
-//     });
-//     res.status(200).json(data);
-//   } else {
-//     res.status(405).json({ message: 'Método não permitido.' });
-//   }
-// }
-
+import { promises as fs } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs/promises';
-import path from 'path';
 
-interface FormData {
-  formData: any[];
-}
+const path = require("path");
 
-const filePath = path.join(process.cwd(), '/src/json/data.json');
+const filePath = path.join(process.cwd(), "/src/json");
 
 const saveFormData = async (data: any) => {
-  const existingData = await getFormData();
-  const formData: FormData = { formData: [...existingData, data] };
-  const jsonData = JSON.stringify(formData);
+  let arr = [];
+  arr.push(await getFormData());
+  const jsonData = JSON.stringify({ formData: [...arr, data] });
   await fs.writeFile(filePath, jsonData);
-};
+  arr.push(jsonData);
+}
 
-const getFormData = async (): Promise<any[]> => {
-  try {
-    const jsonData = await fs.readFile(filePath, 'utf8');
-    const formData: FormData = JSON.parse(jsonData);
-    return formData.formData || [];
-  } catch (error) {
-    return [];
-  }
+const getFormData = async () => {
+  await fs.readFile(filePath + '/data.json', 'utf8');
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -62,7 +23,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     saveFormData(data);
     res.status(200).json({ message: 'Dados salvos com sucesso.' });
   } else if (req.method === 'GET') {
-    const data = await getFormData();
+    const data = getFormData().then((e: any) => {
+      return JSON.parse(e);
+    });
     res.status(200).json(data);
   } else {
     res.status(405).json({ message: 'Método não permitido.' });
