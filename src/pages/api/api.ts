b485d02db418fa155aa91ddfcb7b86615b9d9e55
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-07-08 11:33:18 -0300

    bugfix: refatorando get da api

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,24 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs/promises';
+import { promises as fs } from "fs";
 import path from 'path';
-
-interface FormData {
-  formData: any[];
-}
 
 const filePath = path.join(process.cwd(), '/src/json/data.json');
 
 const saveFormData = async (data: any) => {
   const existingData = await getFormData();
-  const formData: FormData = { formData: [...existingData, data] };
+  const formData = { formData: [...existingData, data] };
   const jsonData = JSON.stringify(formData);
+  console.log(jsonData);
   await fs.writeFile(filePath, jsonData);
 };
 
 const getFormData = async (): Promise<any[]> => {
   try {
     const jsonData = await fs.readFile(filePath, 'utf8');
-    const formData: FormData = JSON.parse(jsonData);
+    const formData = JSON.parse(jsonData);
     return formData.formData || [];
   } catch (error) {
     return [];
@@ -28,6 +25,7 @@ const getFormData = async (): Promise<any[]> => {
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const data = req.body;
+    console.log(data)
     saveFormData(data);
     res.status(200).json({ message: 'Dados salvos com sucesso.' });
   } else if (req.method === 'GET') {
