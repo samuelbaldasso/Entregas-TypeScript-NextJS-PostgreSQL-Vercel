b9e9753c0c8e0a0e6079b123b97f452f9531ff8f
samuelbaldasso <samuelbaldasso93@gmail.com>
2023-07-09 02:48:14 -0300

    bugfix: refatorando post da api

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -3,6 +3,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const path = require("path");
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 const filePath = path.join(process.cwd(), '/tmp/src/json/data.json');
 
 const saveFormData = async (data: any) => {
